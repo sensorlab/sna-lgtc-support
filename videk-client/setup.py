ced15daa4e98c3f849fbbfed1b1a340473887b20
Matevz Vucnik <vucnikm@gmail.com>
2017-08-22 16:34:24 +0200

    [videk-client] Add depends

--- videk-client/setup.py
+++ videk-client/setup.py
@@ -1,9 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='VidekClient',
     version='0.1dev',
     packages=['videkrestclient',],
-    scripts = ['videk-client'],
-    data_files = [('/etc/videk', ['api.key'])]
+    scripts=['videk-client'],
+    data_files=[('/etc/videk', ['api.key'])],
+    install_requires=['requests']
 )
