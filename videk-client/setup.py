abae5dbe8a3a4ca648c8341c54c4e5e995449075
Matevz Vucnik <vucnikm@gmail.com>
2017-08-22 14:02:17 +0200

    [videk-client] Fix conf

--- videk-client/setup.py
+++ videk-client/setup.py
@@ -3,8 +3,7 @@ from distutils.core import setup
 setup(
     name='VidekClient',
     version='0.1dev',
-    packages=['videk_rest_client',],
-    scripts = [
-        'videk-client'
-    ]
+    packages=['videkrestclient',],
+    scripts = ['videk-client'],
+    data_files = [('/etc/videk', ['api.key'])]
 )
